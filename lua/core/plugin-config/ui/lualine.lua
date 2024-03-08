d46958b0cef918b7d5c3190c3c7124d2268808b8
nathan <side_nathan@hotmail.com>
2024-03-07 23:42:48 -0500

    lualine change to global

--- lua/core/plugin-config/ui/lualine.lua
+++ lua/core/plugin-config/ui/lualine.lua
@@ -1,13 +1,20 @@
 local navic = require("nvim-navic");
 
 require("lualine").setup({
-    winbar = {
+    options = {
+        theme = "auto",
+        globalstatus = true,
+    },
+
+    tabline = {
         lualine_a = {
             {
                 "buffers",
                 mode = 4
             }
         },
+    },
+    winbar = {
         lualine_c = {
             {
                 function()
