0c65732f6c18a1966f39a4ac5119415ee9515e8a
nathan <side_nathan@hotmail.com>
2024-04-27 01:06:56 -0400

    get working ts-autotag config to work by actually attaching to config to it rather than base config in lazy that overwrites my working config file setup version

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -73,9 +73,6 @@ local plugins = {
     {
         'windwp/nvim-ts-autotag',
         dependencies = 'nvim-treesitter/nvim-treesitter',
-        config = function()
-        require('nvim-ts-autotag').setup()
-        end,
         lazy = true,
         event = 'VeryLazy',
     },
