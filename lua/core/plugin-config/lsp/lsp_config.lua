51bfe705cb82b8b410d14828a34dde6582fdc5ff
bathan1 <side_nathan@hotmail.com>
2024-07-06 02:44:09 -0700

    camelcase - false

--- lua/core/plugin-config/lsp/lsp_config.lua
+++ lua/core/plugin-config/lsp/lsp_config.lua
@@ -131,7 +131,7 @@ lspconfig.cssmodules_ls.setup {
         client.server_capabilities.definitionProvider = false
     end,
     init_options = {
-        camelCase = true
+        camelCase = false
     }
 }
 
