65b370e4603a73a25c58143ec9eb9abc651684a7
nathan <side_nathan@hotmail.com>
2024-09-19 16:31:27 -0400

    fine microsoft u win

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -31,5 +31,3 @@ vim.keymap.set('n', '<C-h>', '<cmd> TmuxNavigateLeft<CR>')
 vim.keymap.set('n', '<C-j>', '<cmd> TmuxNavigateDown<CR>')
 vim.keymap.set('n', '<C-k>', '<cmd> TmuxNavigateUp<CR>')
 vim.keymap.set('n', '<C-l>', '<cmd> TmuxNavigateRight<CR>')
-
-vim.keymap.set("n", "-", "<cmd>lua require('vscode').call('workbench.files.action.focusFilesExplorer')<CR>")
