c2a4eaff7a2df535abf2682438541062ad4cbe43
bathan1 <side_nathan@hotmail.com>
2025-01-03 11:47:35 -0800

    add more vscode keybindings

--- lua/vscode-config.lua
+++ lua/vscode-config.lua
@@ -8,3 +8,30 @@ vim.keymap.set(
     end,
     { noremap = true, silent = true }
 )
+
+vim.keymap.set(
+    'n',
+    '<space>rn', 
+    function()
+        vscode.call("editor.action.rename")
+    end
+    ,
+    {}
+)
+vim.keymap.set(
+    { 'n', 'v' }, 
+    '<space>ca', 
+    function()
+        vscode.call("editor.action.codeAction")
+    end, 
+    {}
+)
+vim.keymap.set(
+    'n',
+    'gr', 
+    function()
+        vscode.call("editor.action.referenceSearch.trigger")
+    end
+    , 
+    {}
+)
