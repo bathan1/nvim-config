f93116def755a214e0955b3f358a3fd4026b207b
bathan1 <side_nathan@hotmail.com>
2026-03-03 11:48:50 -0500

    Add parent folder label to current buffer name

--- lua/plugin-config/ui/lualine.lua
+++ lua/plugin-config/ui/lualine.lua
@@ -1,5 +1,30 @@
 local navic = require("nvim-navic");
 
+local function smart_filename()
+  local filename = vim.fn.expand("%:t")
+  local parent = vim.fn.expand("%:h:t")
+
+  -- Count open buffers with same filename
+  local count = 0
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) then
+      local bufname = vim.fn.fnamemodify(
+        vim.api.nvim_buf_get_name(buf),
+        ":t"
+      )
+      if bufname == filename then
+        count = count + 1
+      end
+    end
+  end
+
+  if count > 1 then
+    return parent .. "/" .. filename
+  else
+    return filename
+  end
+end
+
 require("lualine").setup({
     options = {
         theme = "auto",
@@ -11,12 +36,12 @@ require("lualine").setup({
             {
                 "buffers",
                 mode = 4
-            }
+            },
         },
     },
     winbar = {
         lualine_c = {
-            "filename",
+            smart_filename,
             {
                 function()
                     return navic.get_location()
