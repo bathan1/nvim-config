dd572d2bb234bebbc7548d90abf3f063cf9f8305
bathan1 <side_nathan@hotmail.com>
2026-07-08 13:00:58 -0400

    Update blink.cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -184,26 +184,47 @@ local plugins = {
             })
         end,
     },
-    { -- optional blink completion source for require statements and module annotations
-        "saghen/blink.cmp",
-        dependencies = { "saghen/blink.lib" },
-        build = function()
-            require("blink.cmp").build():pwait()
-        end,
-        opts = {
-            sources = {
-                -- add lazydev to your completion providers
-                default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-                providers = {
-                    lazydev = {
-                        name = "LazyDev",
-                        module = "lazydev.integrations.blink",
-                        -- make lazydev completions top priority (see `:h blink.cmp`)
-                        score_offset = 100,
-                    },
-                },
-            },
-        },
+    {
+      'saghen/blink.cmp',
+      dependencies = {
+        'saghen/blink.lib',
+        -- optional: provides snippets for the snippet source
+        'rafamadriz/friendly-snippets',
+      },
+      build = function()
+        require('blink.cmp').build({ force = true }):pwait(60000)
+      end,
+    
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.Config
+      opts = {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = { preset = 'default' },
+    
+        -- (Default) Only show the documentation popup when manually triggered
+        completion = { documentation = { auto_show = false } },
+    
+        -- (Default) list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+    
+        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+        -- You may use a lua implementation instead by using `implementation = "lua"`
+        -- See the fuzzy documentation for more information
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+      },
+      opts_extend = { "sources.default" },
     },
     {
         'kristijanhusak/vim-dadbod-ui',
@@ -222,7 +243,7 @@ local plugins = {
             vim.g.db_ui_use_nerd_fonts = 1
         end,
     },
-    { "bathan1/mql" }
+    { "bathan1/sqld" }
 
 }
 
